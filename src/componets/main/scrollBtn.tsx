59c778fd6659d05733b6c49242d700e9d51400d9
atomic <baileyweb04@gmail.com>
2024-05-19 13:28:35 -0400

    fix

--- src/componets/main/scrollBtn.tsx
+++ src/componets/main/scrollBtn.tsx
@@ -1,38 +1,42 @@
 'use client'
 
-import React, {useState} from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 import upArrow from '../../assets/images/icons/functionalIcon/angle-up-solid.svg';
 import Image from 'next/image';
   
-const ScrollButton = () =>{ 
+const ScrollButton = () => {
+  const [visible, setVisible] = useState(false); 
   
-  const [visible, setVisible] = useState(false) 
-  
-  const toggleVisible = () => { 
-    const scrolled = document.documentElement.scrollTop; 
-    if (scrolled > 300){ 
-      setVisible(true) 
-    }  
-    else if (scrolled <= 300){ 
-      setVisible(false) 
-    } 
-  }; 
-  
-  const scrollToTop = () =>{ 
-    window.scrollTo({ 
-      top: 0,  
+  useEffect(() => {
+    const toggleVisible = () => {
+      const scrolled = window.scrollY;
+      if (scrolled > 300) {
+        setVisible(true);
+      } else {
+        setVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', toggleVisible);
+
+    // Cleanup function to remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', toggleVisible);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
       behavior: 'smooth'
-    }); 
-  }; 
-  
-  window.addEventListener('scroll', toggleVisible); 
-  
-  return ( 
-    <div className='scroll-btn'  onClick={scrollToTop}  
-    style={{display: visible ? 'flex' : 'none'}}>
-      <Image src={upArrow} draggable="false" alt="Scroll to top"></Image>
+    });
+  };
+
+  return (
+    <div className='scroll-btn' onClick={scrollToTop} style={{ display: visible ? 'flex' : 'none' }}>
+      <Image src={upArrow} draggable="false" alt="Scroll to top" />
     </div>
   ); 
-} 
-  
-export default ScrollButton; 
+};
+
+export default ScrollButton;
