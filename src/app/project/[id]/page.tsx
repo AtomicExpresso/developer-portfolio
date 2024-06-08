745c2e09f8b78c8892d93cfc0ca866b8fbe67e19
atomic <baileyweb04@gmail.com>
2024-06-08 12:08:16 -0400

    temp fix

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,7 +1,5 @@
 import ProjectComp from "@/componets/project/projectComp";
 import "@/app/styles/project-item.scss";
-import fs from 'fs';
-import path from 'path';
 
 type Params = {
   id: string;
@@ -15,20 +13,3 @@ export default function page({ params }: { params: Params }){
   )
 }
 
-const getProjectsData = () => {
-  const filePath = path.join(process.cwd(), 'public', 'projects.json');
-  const jsonData = fs.readFileSync(filePath, 'utf-8');
-  return JSON.parse(jsonData);
-};
-
-// Implement generateStaticParams
-export async function generateStaticParams() {
-  const projects = getProjectsData();
-
-  // Map the projects to the necessary format
-  const paths = projects.map((project: { id: string }) => ({
-    params: { id: project.id }
-  }));
-
-  return paths;
-}
