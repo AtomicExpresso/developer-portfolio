b0d02e72cf19ac9008028eee8a5764b9ed180cd8
atomic <baileyweb04@gmail.com>
2025-01-08 15:00:03 -0600

    -fix: fixed linting issue

--- src/componets/project/projectComp.tsx
+++ src/componets/project/projectComp.tsx
@@ -1,9 +1,7 @@
 'use client'
 import { usePathname } from "next/navigation";
-import projectItemContent from '../../../public/projectItemContent.json';
 
 import ProjectTitle from "./projectTitle";
-import ProjectContent from "./projectContent";
 
 export const metadata = {
   title: "A list of projects",
@@ -13,13 +11,10 @@ export const metadata = {
 export default function ProjectComp(){
   const path = usePathname();
   const id = path.split('/')[2]; //Gets the ID from the current url
-  const results = projectItemContent.results[Number(id)];
 
   return (
     <div className="blog-page-content">
-      <ProjectTitle results={results}/>
       <hr></hr>
-      <ProjectContent results={results}/>
     </div>
   )
 }
